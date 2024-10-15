c8e313274e91c94d56e09526adc347c9a959e6e3
pablitodon <pavelkazakevich040196@gmail.com>
2024-10-15 13:37:36 +0300

    edit styles

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,25 +1,23 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 import { pagesImage, socilalLinksIcons } from '../../assets';
-
-
 
 const Contacts = () => {
     return (
         <section className={styles.contacts}>
-            <div className={styles.textContainer} >
+            <div className={styles.textContainer}>
                 <h1 className={styles.header}>Контакты:</h1>
-                <ul >
+                <ul>
                     <li className={styles.link}>
                         <img src={socilalLinksIcons.mail} alt="mail" />
-                        <a target='_blank' className={styles.link} href="mailto:pavelkazakevich040196@gmail.com">pavelkazakevich040196@gmail.com</a>
-                    </li >
+                        <a target='_blank' rel="noopener noreferrer" className={styles.link} href="mailto:pavelkazakevich040196@gmail.com">pavelkazakevich040196@gmail.com</a>
+                    </li>
                     <li className={styles.link}>
                         <img src={socilalLinksIcons.vkontakte} alt="vk" />
-                        <a target='_blank' className={styles.link} href="https://vk.com/kazakevich_pavel">ВКонтакте</a>
+                        <a target='_blank' rel="noopener noreferrer" className={styles.link} href="https://vk.com/kazakevich_pavel">ВКонтакте</a>
                     </li>
                     <li className={styles.link}>
                         <img src={socilalLinksIcons.telegram} alt="tg" />
-                        <a target='_blank' className={styles.link} href="https://t.me/donpabloooo">+375297056356</a>
+                        <a target='_blank' rel="noopener noreferrer" className={styles.link} href="https://t.me/donpabloooo">+375297056356</a>
                     </li>
                     <li className={styles.link}>
                         <img src={socilalLinksIcons.whatsapp} alt="whatsapp" />
